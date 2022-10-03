cfba49f49f4429820954c2bd9ff23e6bbb72871d
Obinna <obinna.aguwa@gmail.com>
2022-10-03 10:28:01 +0100

    Initial Commit

---
+++ SwiftUIMobileTask/SwiftUIMobileTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMobileTaskApp.swift
+//  SwiftUIMobileTask
+//
+//  Created by Obinna Aguwa on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMobileTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
